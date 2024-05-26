f0f64c844293ab935e77381f416fe81a61162dd3
lin xu <helado_a@hotmail.com>
2024-05-26 16:53:36 +0200

    longestword

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,31 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  # an array of vowels
+  VOWELS = %w(A E I O U Y)
   def new
-    @letters = Array.new(10) { ('a'..'z').to_a.sample }
+    #an array of 5 vowels + an array of others in radom copy
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
     @letters.shuffle!
   end
 
   def score
-    params[:word]
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letter)
+    @english_word = english_word?(@word)
   end
 
-  def hidden_field_tag
-    <input >
-    if result
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
